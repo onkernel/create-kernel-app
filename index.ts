54b5acbc5bf8163c839fdf39ca2b057e21da7392
Catherine Jue <catherine.djue@gmail.com>
2025-06-08 05:50:43 -0700

    Add instructions to CLI (#13)
    * Add instructions to CLI
    
    * Remove unnecessary file
    
    * Update for cu

--- index.ts
+++ index.ts
@@ -101,6 +101,32 @@ const INVOKE_SAMPLES: Record<
   },
 };
 
+const REGISTERED_APP_NAMES: Record<
+  LanguageKey,
+  Partial<Record<TemplateKey, string>>
+> = {
+  [LANGUAGE_TYPESCRIPT]: {
+    [TEMPLATE_SAMPLE_APP]:
+      'ts-basic',
+    [TEMPLATE_STAGEHAND]:
+      'ts-stagehand',
+    [TEMPLATE_PERSISTENT_BROWSER]:
+      'ts-persistent-browser',
+    [TEMPLATE_COMPUTER_USE]:
+      'ts-cu',
+  },
+  [LANGUAGE_PYTHON]: {
+    [TEMPLATE_SAMPLE_APP]:
+      'python-basic',
+    [TEMPLATE_BROWSER_USE]:
+      'python-bu',
+    [TEMPLATE_PERSISTENT_BROWSER]:
+      'python-persistent-browser',
+    [TEMPLATE_COMPUTER_USE]:
+      'python-cu',
+  },
+};
+
 const CONFIG = {
   templateBasePath: path.resolve(__dirname, "../templates"),
   defaultAppName: "my-kernel-app",
@@ -330,6 +356,7 @@ function printNextSteps(
 
 Next steps:
   cd ${appName}
+  # Request early access for an API key: https://waitlist.onkernel.com/r/mZW2zz
   export KERNEL_API_KEY=<YOUR_API_KEY>
   ${
     language === LANGUAGE_PYTHON
@@ -338,6 +365,7 @@ Next steps:
   }
   ${deployCommand}
   ${INVOKE_SAMPLES[language][template]}
+  kernel logs ${REGISTERED_APP_NAMES[language][template]} --follow
   `)
   );
 }
